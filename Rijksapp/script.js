bb115ac4374d3452ee039987d7b4929c38793a18
Nigel Fijnheer <34651169+Neinno@users.noreply.github.com>
2023-02-16 19:37:44 +0100

    fetch working

--- Rijksapp/script.js
+++ Rijksapp/script.js
@@ -1,11 +1,31 @@
+const artContainer = document.querySelector('main > section:nth-of-type(2)')
+
+fetchData();
+
+
 function fetchData() {
     
-    const url = "https://www.rijksmuseum.nl/api/nl/collection?key="+apikey;
     const apikey = "0TyrFANJ";
+    const url = `https://www.rijksmuseum.nl/api/nl/collection?key=`+apikey+`&ps=0`;
     
     const data = fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            data.artObjects.forEach(data => {
+
+                const createFigure = document.createElement("figure");
+                artContainer.appendChild(createFigure);
+
+                const createFigcaption = document.createElement("figcaption");
+                createFigcaption.textContent = data.title;
+                createFigure.appendChild(createFigcaption);
+
+                const createImage = document.createElement("img");
+                createImage.src = data.webImage.url;
+                createFigure.appendChild(createImage);
+
+            })
+
+            console.log(data)      
         })
 }
