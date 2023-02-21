0394fc62655b0b651fa05c2ea3c566e3722e2945
Nigel Fijnheer <34651169+Neinno@users.noreply.github.com>
2023-02-21 22:54:52 +0100

    Error state fix
    Error state is now working

--- Rijksapp/loader.js
+++ Rijksapp/loader.js
@@ -1,7 +1,9 @@
 export function loader() {
-    const mainContainer = document.querySelector('main > section:nth-of-type(2)')
+    const artContainer = document.querySelector('main > section:nth-of-type(2)')
+
+    artContainer.textContent="";
 
     const createLoading = document.createElement("div")
     createLoading.setAttribute("id", "loading")
-    mainContainer.appendChild(createLoading);
+    artContainer.appendChild(createLoading);
 };
